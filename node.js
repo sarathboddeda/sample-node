8994e5c354dfea74cfb3fb2b45c133e3cbde2243
sarath <sarathboddeda@gmail.com>
2024-11-12 20:20:31 +0000

    Delete node.js

--- node.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello, World! This is a sample app.');
-});
-app.listen(3000, () => console.log('Server is running on port 3000.'));
