d92812ed001b5d7d66621c4a61a85db5b92fdcc8
Sarath Boddeda <sarathboddeda@gmail.com>
2024-11-15 19:29:55 +0000

    added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello, World! This is a sample app.');
+  res.send('Hello, World! This is a sample app by sarath boddeda');
 });
 app.listen(3000, () => console.log('Server is running on port 3000.'));
